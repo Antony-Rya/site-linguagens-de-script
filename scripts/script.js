b6cee9c38f0932cf44c4669f59bfa1edd1a3c685
Antony-Rya <159479364+Antony-Rya@users.noreply.github.com>
2024-03-06 19:54:38 +0000

    Alterações

--- scripts/script.js
+++ scripts/script.js
@@ -18,6 +18,22 @@
 //     window.location.href = "../index.html"
 // }
 
-var preco = prompt("Qual preco do produto")
-var mensagem = (preco<=20) ? "Barato...": "Caro"
-alert(mensagem)
+// var preco = prompt("Qual preco do produto")
+// var mensagem = (preco<=20) ? "Barato...": "Caro"
+// alert(mensagem)
+
+var mes =  prompt("Digite o mes. \n 1-Janeiro \n 2-Fevereiro \n 3-Março")
+switch(mes){
+    case 1: 
+        alert("Janeiro")
+        break;
+    case 2: 
+        alert("Fevereiro")
+        break;
+    case 3: 
+        alert("Março")
+        break;
+    default:
+        alert("Mês invalido")
+        break;
+}
