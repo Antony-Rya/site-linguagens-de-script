7a9bd804f68b97b93044eaa8bf57b3af953e1b89
Antony-Rya <159479364+Antony-Rya@users.noreply.github.com>
2024-03-06 19:34:54 +0000

    Alterações

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,23 @@
-var nome = prompt("Qual seu nome?");
-document.getElementById("descricao").innerHTML += nome;
+// var condicao = false;
+// var manha = true
+// var tarde = false
+// var noite = false
+// if(manha){
+//     alert("Manhã")
+// }else if(tarde){
+//     alert("Tarde")
+// }else if(noite){
+//     alert("Noite")
+// }
+
+// var nome = prompt("Qual seu nome?")
+// var resposta = confirm(nome + ", você gostaria de continuar?")
+// if(resposta){
+//     alert("Seja bem vindo")
+// }else{
+//     window.location.href = "../index.html"
+// }
+
+var preco = prompt("Qual preco do produto")
+var mensagem = (preco<=20) ? "Barato...": "Caro"
+alert(mensagem)
