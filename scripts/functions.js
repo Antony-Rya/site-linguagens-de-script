4c42111ca74b29cf31889b8cd0c68de3116655bc
Antony-Rya <159479364+Antony-Rya@users.noreply.github.com>
2024-03-11 20:17:52 +0000

    salvr aqui né

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,7 +1,27 @@
-function imprimirMsgA(){
-    alert('Você clicou no botão A.');
-}
-function imprimirMsgB(){
-    alert('Você clicou no botão B.');
+function imprimirMsg(texto){
+    alert('Você clicou no botão ' + texto);
+
 }
 
+function login(){
+    const emailCorreto = "ryanzinhoreidelas@gmail.com"
+    const senhaCorreta = "senha"
+
+    var email = document.getElementById("email").value;
+    var senha = document.getElementById("pwd").value;
+
+    if(email == emailCorreto){
+        if(senha == senhaCorreta){
+          alert("Email e senha incorreto")
+    }else{
+        alert("Email incorreto")
+    }
+
+    // if(email == emailCorreto && senha == senhaCorreta){
+    //     alert("Login bem sucedido")
+        
+    // }else{
+    //     alert("Login incorreto")
+    // }
+   
+}
