a7b124c19595b9c8f4930866c7e5c88562085c10
Antony-Rya <159479364+Antony-Rya@users.noreply.github.com>
2024-03-13 20:00:53 +0000

    alteração

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,14 +1,12 @@
 function imprimirMsg(texto){
+    window.location.href = "../index.html";
 }
 
 function login(){
-    const emailCorreto = "ryanzinhoreidelas@gmail.com"
-    const senhaCorreta = "senha"
+    // const emailCorreto = "ryanzinhoreidelas@gmail.com"
+    // const senhaCorreta = "senha"
 
-    var email = document.getElementById("email").value;
-    var senha = document.getElementById("pwd").value;
-
-    if(senhaCorreta == senhaCorreta){ 
-          window.location.href = "../index.html";
-    }
+    // var email = document.getElementById("email").value;
+    // var senha = document.getElementById("pwd").value;
+    window.location.href = "../index.html";
 }
