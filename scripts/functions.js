ec98e69336b131d27a41cd2e110ca33c59c0b649
Antony-Rya <159479364+Antony-Rya@users.noreply.github.com>
2024-03-18 20:19:44 +0000

    Alterações

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,12 +1,17 @@
 function imprimirMsg(texto){
-    window.location.href = "../index.html";
+    alert("Você apertou o botão: " + texto)
+    // window.location.href = "../index.html";
+    window.location.replace("../index.html");
 }
 
 function login(){
+    alert("👍")
     // const emailCorreto = "ryanzinhoreidelas@gmail.com"
     // const senhaCorreta = "senha"
 
     // var email = document.getElementById("email").value;
     // var senha = document.getElementById("pwd").value;
     window.location.href = "../index.html";
+
+
 }
